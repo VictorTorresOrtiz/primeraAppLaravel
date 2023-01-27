62cabe8c09320e1c9477349a7e11c7b791beccaf
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-01-27 09:41:08 +0100

    updates

--- laravel/app/Models/student.php
+++ laravel/app/Models/student.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class student extends Model
+class Student extends Model
 {
     use HasFactory;
+
+    public function student(){
+        return $this->belongsToMany(Subject::class);
+    }
+
+
+
+    
 }
